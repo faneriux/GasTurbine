e76b879cbeca4beb6698a42e490be5a625aafb5d
Domantas Gircys <domgirc@gmail.com>
2026-07-29 22:38:24 +0100

    SimulationClock prep

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,24 @@
 
 int main(){
 
-std::cout<<"Gas Turbine Simulation \n";
+constexpr double time_step_s{0.01};
+constexpr double simulation_duration_s{5.0};
+
+double simulation_time_s{0.0};
+
+std::size_t iteration{0};
+
+while (simulation_time_s < simulation_duration_s)
+{
+
+    if (iteration % 100 == 0)
+    {
+    std::cout << simulation_time_s << '\n';
+    }
+
+    ++iteration;
+    simulation_time_s = static_cast<double>(iteration) * time_step_s;
+}
 
     return 0;
 }
